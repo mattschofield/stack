ec85d4d82b2f07d2bdf8cf5f136c5b84c8c2f91c
Matt Schofield <matt.schofield.102@gmail.com>
2016-08-20 16:28:18 +0100

    adds rds-instance stack

---
+++ rds-instance/main.tf
@@ -0,0 +1,68 @@
+variable "allocated_storage" {
+  description = "The amount of storage to allocate to this RDS instance"
+  default     = 10
+}
+
+variable "engine" {
+  default = "postgres"
+  description = "Engine type, example values mysql, postgres"
+}
+
+variable "engine_version" {
+  description = "Engine version"
+  default = {
+    mysql = "5.6.22"
+    postgres = "9.4.1"
+  }
+}
+
+# db.m3.large
+variable "instance_class" {
+  description = "The size of database instance to use"
+  default     = "db.t1.micro"
+}
+
+variable "name" {
+  description = "The name will be used to prefix and tag the resources, e.g mydb"
+}
+
+variable "master_username" {
+  description = "The master user username"
+}
+
+variable "master_password" {
+  description = "The master user password"
+}
+
+variable "subnet_ids" {
+  description = "A comma-separated list of subnet IDs"
+}
+
+resource "aws_db_subnet_group" "main" {
+  name        = "${var.name}"
+  description = "RDS instance subnet group"
+  subnet_ids  = ["${split(",", var.subnet_ids)}"]
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage      = "${var.allocated_storage}"
+  engine                 = "${var.engine}"
+  engine_version         = "${lookup(var.engine_version, var.engine)}"
+  instance_class         = "${var.instance_class}"
+  name                   = "${var.name}"
+  username               = "${var.master_username}"
+  password               = "${var.master_password}"
+  db_subnet_group_name   = "${aws_db_subnet_group.main.id}"
+  multi_az               = true
+}
+
+# OUTPUTS
+output "subnet_group" {
+  value = "${aws_db_subnet_group.default.name}"
+}
+output "db_instance_id" {
+  value = "${aws_db_instance.default.id}"
+}
+output "db_instance_address" {
+  value = "${aws_db_instance.default.address}"
+}
